71cd633edd59ca07bb80097fd3cb72b187e54edd
jessicawycha <jessicawycha@yahoo.com>
2021-02-24 11:11:00 -0600

    Created Exercise and Workout objects

---
+++ app/src/main/java/com/kernelpanic/sovita/Exercise.kt
@@ -0,0 +1,16 @@
+package com.kernelpanic.sovita
+// Add an image as parameter later on
+//This holds the exercise
+data class Exercise (val name : String, val catagory : String) {
+    val exerciseName = name
+    val exerciseCategory = catagory
+    val reps = 0
+    val sets = 0
+    val time = 0
+
+
+
+
+    //Add class functions
+
+}
---
+++ app/src/main/java/com/kernelpanic/sovita/NewWorkout.kt
@@ -0,0 +1,11 @@
+package com.kernelpanic.sovita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewWorkout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_workout)
+    }
+}
