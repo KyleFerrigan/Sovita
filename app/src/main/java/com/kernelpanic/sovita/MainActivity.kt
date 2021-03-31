16a66d5e6fa45eda24e29c8c86e9c2d206b3179f
Christian Logan <cjlogan@noctrl.edu>
2021-03-31 11:34:20 -0500

    Changed the primary and secondary color schemes for the app.

--- app/src/main/java/com/kernelpanic/sovita/MainActivity.kt
+++ app/src/main/java/com/kernelpanic/sovita/MainActivity.kt
@@ -1,6 +1,44 @@
 package com.kernelpanic.sovita
 
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.InputStream
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_exercise, R.id.navigation_chat, R.id.navigation_meals))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+    }
+
+
+}
+
+
+/*
+package com.kernelpanic.sovita
+
+import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,6 +50,10 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.SignInButton
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -61,3 +103,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+*/
