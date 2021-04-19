7c93698dad22d98d897b25b2aace38cca40e8929
kbferrigan <kbferrigan@outlook.com>
2021-04-19 11:48:08 -0500

    OKHTTP Progress

--- app/src/main/java/com/kernelpanic/sovita/MainActivity.kt
+++ app/src/main/java/com/kernelpanic/sovita/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import okhttp3.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +23,40 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_exercise, R.id.navigation_chat, R.id.navigation_meals))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+}
+class okhttptest{
+    OkHttpClient client = new OkHttpClient();
+
+    JsonObject postData = new JsonObject()
+    postData.addProperty("name", "morpheus");
+    postData.addProperty("job", "leader");
+
+    final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    RequestBody postBody = RequestBody.create(JSON, postData.toString());
+    Request post = new Request.Builder()
+    .url("https://reqres.in/api/users")
+    .post(postBody)
+    .build();
+
+    client.newCall(post).enqueue(new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            e.printStackTrace();
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) {
+            try {
+                ResponseBody responseBody = response.body();
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                Log.i("data", responseBody.string());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
