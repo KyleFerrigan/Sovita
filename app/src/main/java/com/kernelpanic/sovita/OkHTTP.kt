bb39de140131fd597f5cb90c254b2b412a288754
Kyle Ferrigan <kbferrigan@outlook.com>
2021-04-21 13:34:15 -0500

    okHTTP progress

--- app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
+++ app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
@@ -7,6 +7,25 @@ import java.io.*
 public fun GetRequest(urlIn: String){
 
 }
+public fun PostWorkoutData(workoutIDIn: String, userIDIn: String, containsExercisesIn: String) {//Input should be as follows Url,IDName,ID,parameterName,ParameterData
+    val client = OkHttpClient()
+
+    val formBody: FormBody = okhttp3.FormBody.Builder()
+        .add("WorkoutID", workoutIDIn)    //id info
+        .add("UserID", userIDIn) //data to change
+        .add("ContainsExercises", containsExercisesIn)
+        .build()
+
+    val request: Request = okhttp3.Request.Builder()
+        .url("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Exercises")//url to execute on
+        .post(formBody)
+        .build()
+
+    val call = client.newCall(request)
+    val response = call.execute()
+}
+
+//Modular attempt below, not gonna happen
 public fun PostData(urlIn: String, idName: String, idIn: String, paramName: String, valueIn: String) {//Input should be as follows Url,IDName,ID,parameterName,ParameterData
     val client = OkHttpClient()
     val formBody: FormBody = okhttp3.FormBody.Builder()
@@ -29,5 +48,8 @@ public fun PostData(urlIn: String, idName: String, idIn: String, paramName: Stri
 }
 
 fun main(){
-  PostData("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Exercises", "ExerciseID", "93", "ExerciseName", "ServerTest");
+    PostWorkoutData("2", "3", "3,2,5");
+    //PostData("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Exercises", "ExerciseID", "93", "ExerciseName", "ServerTest");
 }
+
+
