d2b8e1efd8f4ba8fa53c63f1f131f8f04242f5a7
Kyle Ferrigan <kbferrigan@outlook.com>
2021-04-21 12:34:24 -0500

    okHTTP progress

--- app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
+++ app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
@@ -1,5 +1,31 @@
 package com.kernelpanic.sovita
+
 import okhttp3.*
+import java.io.*
+
 
 class OkHTTP {
+    fun GetRequest(urlIn: String){
+
+    }
+    fun PostData(urlIn: String, idName: String, idIn: String, nameIn: String, valueIn: String){
+        val client = OkHttpClient()
+        val formBody: FormBody = okhttp3.FormBody.Builder()
+            .add(idName,idIn)    //id info
+            .add(nameIn,valueIn) //data to change
+
+            .build()
+        val request: Request = okhttp3.Request.Builder()
+            .url(urlIn)//url to execute on
+            .post(formBody)
+            .build()
+
+        try {
+            val response = client.newCall(request).execute()
+
+            // Do something with the response.
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
