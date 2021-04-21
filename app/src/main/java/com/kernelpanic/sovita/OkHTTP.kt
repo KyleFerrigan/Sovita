f6ea25d634d84904f328dd4d30bf62f3f51b5d11
Kyle Ferrigan <kbferrigan@outlook.com>
2021-04-21 13:02:46 -0500

    okHTTP progress

--- app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
+++ app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
@@ -1,31 +1,33 @@
 package com.kernelpanic.sovita
-
 import okhttp3.*
 import java.io.*
 
 
-class OkHTTP {
-    fun GetRequest(urlIn: String){
 
+public fun GetRequest(urlIn: String){
+
+}
+public fun PostData(urlIn: String, idName: String, idIn: String, paramName: String, valueIn: String){//Input should be as follows Url,IDName,ID,parameterName,ParameterData
+    val client = OkHttpClient()
+    val formBody: FormBody = okhttp3.FormBody.Builder()
+        .add(idName,idIn)    //id info
+        .add(paramName,valueIn) //data to change
+        .build()
+
+    val request: Request = okhttp3.Request.Builder()
+        .url(urlIn)//url to execute on
+        .post(formBody)
+        .build()
+
+    try {
+        val response = client.newCall(request).execute()
+
+        // Do something with the response.
+    } catch (e: IOException) {
+        e.printStackTrace()
     }
-    fun PostData(urlIn: String, idName: String, idIn: String, nameIn: String, valueIn: String){
-        val client = OkHttpClient()
-        val formBody: FormBody = okhttp3.FormBody.Builder()
-            .add(idName,idIn)    //id info
-            .add(nameIn,valueIn) //data to change
+}
 
-            .build()
-        val request: Request = okhttp3.Request.Builder()
-            .url(urlIn)//url to execute on
-            .post(formBody)
-            .build()
-
-        try {
-            val response = client.newCall(request).execute()
-
-            // Do something with the response.
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
+fun main(){
+    PostData("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Exercises", "ExerciseID", "93", "ExerciseName", "ServerTest")
 }
