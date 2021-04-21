85bdba0813d78b6a3feb65e08f1f01ef78bf56f4
Kyle Ferrigan <kbferrigan@outlook.com>
2021-04-21 13:06:03 -0500

    okHTTP progress

--- app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
+++ app/src/main/java/com/kernelpanic/sovita/OkHTTP.kt
@@ -7,11 +7,11 @@ import java.io.*
 public fun GetRequest(urlIn: String){
 
 }
-public fun PostData(urlIn: String, idName: String, idIn: String, paramName: String, valueIn: String){//Input should be as follows Url,IDName,ID,parameterName,ParameterData
+public fun PostData(urlIn: String, idName: String, idIn: String, paramName: String, valueIn: String) {//Input should be as follows Url,IDName,ID,parameterName,ParameterData
     val client = OkHttpClient()
     val formBody: FormBody = okhttp3.FormBody.Builder()
-        .add(idName,idIn)    //id info
-        .add(paramName,valueIn) //data to change
+        .add(idName, idIn)    //id info
+        .add(paramName, valueIn) //data to change
         .build()
 
     val request: Request = okhttp3.Request.Builder()
@@ -29,5 +29,5 @@ public fun PostData(urlIn: String, idName: String, idIn: String, paramName: Stri
 }
 
 fun main(){
-    PostData("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Exercises", "ExerciseID", "93", "ExerciseName", "ServerTest")
+  PostData("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Exercises", "ExerciseID", "93", "ExerciseName", "ServerTest");
 }
