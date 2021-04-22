d14da781297b7a25c4a4e1aef6f18c0b020f36bf
Christian Logan <cjlogan@noctrl.edu>
2021-04-22 16:54:18 -0500

    Created a new screen along with some other stuff. This was a fucking mess

--- app/src/main/java/com/kernelpanic/sovita/WorkoutScreen.kt
+++ app/src/main/java/com/kernelpanic/sovita/WorkoutScreen.kt
@@ -1,5 +1,95 @@
 package com.kernelpanic.sovita
 
-class WorkoutScreen {
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+
+class WorkoutScreen : AppCompatActivity() {
+    private val START_TIME_IN_MILLIS: Long = 600000
+    private var mTextViewCountDown: TextView? = null
+    private var mButtonStartPause: Button? = null
+    private var mButtonReset: Button? = null
+    private var mCountDownTimer: CountDownTimer? = null
+    private var mTimerRunning = false
+    private var mTimeLeftInMillis = START_TIME_IN_MILLIS
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_screen)
+        //setSupportActionBar(findViewById(R.id.toolbar))
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause?.run {
+            setOnClickListener(View.OnClickListener() {
+                fun onClick() {
+                    if (mTimerRunning) {
+                        pauseTimer()
+                    } else {
+                        startTimer()
+                    }
+                }
+            })
+        }
+        mButtonReset?.run {
+            setOnClickListener(View.OnClickListener() {
+                fun onClick() {
+                    resetTimer()
+                }
+            })
+        }
+        updateCountDownText();
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun startTimer() { //start timer
+        mCountDownTimer = object : CountDownTimer(mTimeLeftInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                mTimeLeftInMillis = millisUntilFinished
+                updateCountDownText()
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onFinish() {
+                mTimerRunning = false
+                mButtonStartPause!!.text = "Start"
+                mButtonStartPause!!.visibility = View.INVISIBLE
+                mButtonReset!!.visibility = View.VISIBLE
+            }
+        }.start()
+        mTimerRunning = true
+        mButtonStartPause!!.text = "pause"
+        mButtonReset!!.visibility = View.INVISIBLE
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun pauseTimer() { //pause timer
+        mCountDownTimer!!.cancel()
+        mTimerRunning = false
+        mButtonStartPause!!.text = "Start"
+        mButtonReset!!.visibility = View.VISIBLE
+    }
+
+    fun resetTimer() { //reset timer
+        mTimeLeftInMillis = START_TIME_IN_MILLIS
+        updateCountDownText()
+        mButtonReset!!.visibility = View.INVISIBLE
+        mButtonStartPause!!.visibility = View.VISIBLE
+    }
+
+    fun updateCountDownText() { //updateCountDownText
+        val minutes = (mTimeLeftInMillis / 1000).toInt() / 60
+        val seconds = (mTimeLeftInMillis / 1000).toInt() % 60
+        val timeLeftFormatted: String = java.lang.String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+        mTextViewCountDown!!.text = timeLeftFormatted
+    }
 
 }
