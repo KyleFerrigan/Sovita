75d12ca6429a490033eaf0bfb438422e2d0a71d1
jessicawycha <jessicawycha@yahoo.com>
2021-04-22 16:29:21 -0500

    Can now pass Workout Object, just need to save to database

---
+++ app/src/main/java/com/kernelpanic/sovita/WorkoutScreen.kt
@@ -0,0 +1,22 @@
+package com.kernelpanic.sovita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
+import java.net.URL
+
+class WorkoutScreen : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_screen)
+
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/kernelpanic/sovita/ui/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/kernelpanic/sovita/ui/exercise/ExerciseFragment.kt
@@ -7,10 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -22,7 +19,8 @@ import com.kernelpanic.sovita.Workout
 import org.jetbrains.anko.activityUiThread
 import org.jetbrains.anko.doAsync
 import java.net.URL
-
+import android.widget.ArrayAdapter
+import org.jetbrains.anko.uiThread
 
 
 class ExerciseFragment: Fragment()  {
@@ -31,11 +29,18 @@ class ExerciseFragment: Fragment()  {
     private lateinit var exerciseTextView: TextView
     private lateinit var workoutButton: FloatingActionButton
 
+    private lateinit var workout: Workout
+    private lateinit var userWorkouts: ArrayList<Workout>
+    private lateinit var listWorkouts:ListView
+
 
     // fun OnCreate(savedInstanceState: Bundle?) {
      //   super.onCreate(savedInstanceState)
 
     //}
+
+
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -46,6 +51,19 @@ class ExerciseFragment: Fragment()  {
 
         val view = inflater.inflate(R.layout.fragment_exercise, container, false)
 
+        val workoutNames = ArrayList<String>()
+        listWorkouts = view.findViewById(R.id.workout_list)
+
+        //This will pull all the users workouts
+        doAsychcall("http://ec2-13-58-150-155.us-east-2.compute.amazonaws.com:3000/Workouts/userid/3",workoutNames,listWorkouts)
+
+        println("Final Workout Names")
+        for (i in 0..(workoutNames.size-1)){
+            println(workoutNames[i])
+        }
+
+
+
 
         //This makes the + button on the exercise screen work so that it leads you to the new workout screen
         val workoutButton = view?.findViewById<FloatingActionButton>(R.id.fabExHome)
@@ -54,6 +72,9 @@ class ExerciseFragment: Fragment()  {
             startActivity(intent)
         }
 
+
+
+
         //I don't know what this is for now so I commented it out -- exerciseViewModel.text.observe(viewLifecycleOwner, Observer {
         //})
 
@@ -61,4 +82,101 @@ class ExerciseFragment: Fragment()  {
 
         return view
         }
+
+    private fun doAsychcall(url: String, workoutName: ArrayList<String>, listworkouts: ListView) {
+        doAsync() {
+            println("Here!!!")
+            var result = URL(url).readText()
+            //URL(url).apply { }
+
+            println("Result")
+            println(result)
+            val workoutInfo = ArrayList<String>()
+
+            result = result.removeRange(0, 40) //Gets rid of the header
+            result = result.removeRange(
+                result.length - 3,
+                result.length
+            ) //Gets rid of the extra }]} at the end
+            println("Result after getting rid of extra")
+            println(result)
+
+            var count = 0 //Holds the number of workouts there are
+
+            val split_result = result.split("},{\"").toTypedArray() //Splits then up by row
+            for (i in split_result) {
+                val exercise_info = i.split(",\"").toTypedArray() //Splits then up by row
+                count += 1
+                for (j in exercise_info) {
+                    val exercise_info2 = j.split("\":").toTypedArray() //Splits then up by row
+                    for (k in exercise_info2) {
+                        println("K")
+                        println(k)
+                        workoutInfo.add(k)
+                    }
+                }
+            }
+
+            println("Count Final")
+            println(count)
+
+            var idSlot = 1
+            var useridSlot = 3
+            var exercisesSlot = 5
+            var repsSlot = 7
+            var timeSlot = 9
+            var nameSlot = 11
+
+            //13,15,17,19,21,23 add 12
+
+
+            /*for(i in 0..count) {
+                val exercises = ArrayList<Exercise>()
+                for (j in 0..(workoutInfo[exercisesSlot].length-5)) {
+                    val tempexercise = Exercise()
+                }
+
+
+                workouts.add(Workout(workoutInfo[nameSlot]))
+                //Name [11], Exercises
+            }*/
+            //val workoutNames = ArrayList<String>()
+            for(i in 0..count-1){
+                println("For loop")
+
+                val name = workoutInfo[nameSlot]
+                name.removeRange(0,1)
+                name.removeRange(name.length-1,name.length)
+                workoutName.add(name)
+                println("Name added:" + name)
+                //println(name)
+                //println("Name Slot")
+                nameSlot += 12
+                //print(nameSlot)
+            }
+            uiThread {
+                println("Jessica is hungry")
+                val adapter = activity?.let {
+                    ArrayAdapter(
+                        it,
+                        android.R.layout.simple_list_item_1,workoutName
+                    )
+                }
+
+                val textView = view?.findViewById<TextView>(R.id.test)
+
+                listWorkouts.adapter = adapter
+                listWorkouts.onItemClickListener =
+                    AdapterView.OnItemClickListener { parent, view, position, id ->
+                        val selectedItemText = parent.getItemAtPosition(position)
+                        if (textView != null) {
+                            textView.text = "Selected : $selectedItemText"
+                        }
+                    }
+            }
+        }
+    }
+
+
+
 }
