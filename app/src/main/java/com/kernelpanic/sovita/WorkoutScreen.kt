720f5caba95a1600f3ad4ed5e41ad313bb7dd8c5
jessicawycha <jessicawycha@yahoo.com>
2021-04-27 15:45:56 -0500

    Loads images now!!

--- app/src/main/java/com/kernelpanic/sovita/WorkoutScreen.kt
+++ app/src/main/java/com/kernelpanic/sovita/WorkoutScreen.kt
@@ -1,13 +1,18 @@
 package com.kernelpanic.sovita
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.TextView
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import java.net.URL
@@ -25,6 +30,8 @@ class WorkoutScreen : AppCompatActivity() {
     private var mTimerRunning = false
     private var mTimeLeftInMillis = START_TIME_IN_MILLIS
     private lateinit var exercises : ArrayList<Exercise>
+    //private lateinit var image : ImageView
+    private lateinit var image : WebView
 
     //private lateinit var
 
@@ -43,6 +50,16 @@ class WorkoutScreen : AppCompatActivity() {
         //println("Workout exercises: " + workout.getExerciseIDs())
         //println("Reps: " + workout.getExerciseReps())
         //println("TIme: " + workout.getExerciseTimes())
+        image = findViewById(R.id.webView)
+        image.settings.setJavaScriptEnabled(true)
+
+
+        //Need to get rid of the quotes I don't remember where the quotes are..idk this works though
+        var link = workout.getExerciseImage(0).removeRange(0,1)
+        link = link.removeRange(link.length-1,link.length)
+
+        image.loadUrl(link)
+
 
 
         mTextViewCountDown = findViewById(R.id.text_timer_reps)
@@ -111,6 +128,28 @@ class WorkoutScreen : AppCompatActivity() {
         val seconds = (mTimeLeftInMillis / 1000).toInt() % 60
         val timeLeftFormatted: String = java.lang.String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
         mTextViewCountDown!!.text = timeLeftFormatted
+    }
+
+    private inner class DownloadImageFromInternet(var imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
+        init {
+            Toast.makeText(applicationContext, "Please wait, it may take a few minute...",     Toast.LENGTH_SHORT).show()
+        }
+        override fun doInBackground(vararg urls: String): Bitmap? {
+            val imageURL = urls[0]
+            var image: Bitmap? = null
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+            }
+            catch (e: Exception) {
+                Log.e("Error Message", e.message.toString())
+                e.printStackTrace()
+            }
+            return image
+        }
+        override fun onPostExecute(result: Bitmap?) {
+            imageView.setImageBitmap(result)
+        }
     }
 
 }//end WorkoutScreen class
